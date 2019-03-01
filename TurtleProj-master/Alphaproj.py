34564954fb6a9649eeb5a8356bde858b980c0818
t3bol90 <43676770+t3bol90@users.noreply.github.com>
2019-03-01 19:18:23 +0700

    Delete Alphaproj.py

--- TurtleProj-master/Alphaproj.py
+++
@@ -1,8 +0,0 @@
-import time
-from construct import createRoad, createTurtles, randomTurtles
-
-
-length = createRoad.create()
-t = createTurtles.create()
-time.sleep(1)
-randomTurtles.maktIMove(t, length)
