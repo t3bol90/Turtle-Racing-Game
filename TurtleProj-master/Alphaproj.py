5553f0b236544eb7b9533d008304dceadf674fe0
t3bol90 <43676770+t3bol90@users.noreply.github.com>
2019-03-01 19:15:32 +0700

    Add files via upload

---
+++ TurtleProj-master/Alphaproj.py
@@ -0,0 +1,8 @@
+import time
+from construct import createRoad, createTurtles, randomTurtles
+
+
+length = createRoad.create()
+t = createTurtles.create()
+time.sleep(1)
+randomTurtles.maktIMove(t, length)
